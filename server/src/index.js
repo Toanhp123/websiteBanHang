17cb437c1ab6dd900d93835db8087a3c43d2475c
Toanhp123 <toanpham12102003@gmail.com>
2025-10-08 15:27:31 +0700

    sửa lỗi cors không nhận host front end

--- server/src/index.js
+++ server/src/index.js
@@ -1,41 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { PORT } = require('./configs/env.config');
+const { PORT } = require("./configs/env.config");
 
-const path = require('path');
-const cors = require('cors');
-const express = require('express');
-const route = require('./routes/index.route');
-const cookieParser = require('cookie-parser');
-const sequelize = require('./configs/database.config');
-const errorHandler = require('./middlewares/errorHandler.middleware');
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const route = require("./routes/index.route");
+const cookieParser = require("cookie-parser");
+const sequelize = require("./configs/database.config");
+const errorHandler = require("./middlewares/errorHandler.middleware");
 
 const port = PORT;
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 app.use(
-    cors({
-        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-        credentials: true,
-    }),
+	cors({
+		origin: [
+			"http://localhost:5173",
+			"http://127.0.0.1:5173",
+			"https://website-ban-hang-iota.vercel.app",
+		],
+		credentials: true,
+	})
 );
 
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 
 // Check connect Database
 sequelize
-    .authenticate()
-    .then(() => console.log('Kết nối MySQL thành công'))
-    .catch((err) => console.error('Lỗi kết nối MySQL:', err));
+	.authenticate()
+	.then(() => console.log("Kết nối MySQL thành công"))
+	.catch((err) => console.error("Lỗi kết nối MySQL:", err));
 
 // Route init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+	console.log(`Server is running at http://localhost:${port}`);
 });
 
 app.use(errorHandler);
